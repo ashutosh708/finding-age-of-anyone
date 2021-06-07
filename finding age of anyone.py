08609caec19fa720013a39398322868e128a3eb1
ashutosh708 <85440783+ashutosh708@users.noreply.github.com>
2021-06-07 10:00:42 +0530

    Add files via upload

---
+++ finding age of anyone.py
@@ -0,0 +1,17 @@
+d=True
+while d==True:
+    print("Welcome let's find age of anyone")
+    b=int(input("Enter the year in which you were born"))
+    c=int(input("enter the present year"))
+    print("Your age = ",c-b)
+
+    e=input("Do you want to repeat the program:yes/no").lower()
+    if e=='yes':
+        continue
+    else:
+        exit()
+
+
+
+
+    
